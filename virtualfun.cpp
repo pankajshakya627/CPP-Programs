ad3c2ab3a885091bd481bb527fb5a615512ddf1d
Pankaj Shakya <39346757+pankajshakya627@users.noreply.github.com>
2019-01-04 18:50:49 +0530

    Add files via upload

---
+++ virtualfun.cpp
@@ -0,0 +1,26 @@
+//
+#include<iostream>
+using namespace std;
+class A
+{
+public:
+    virtual void display()
+    {
+        cout<<"This is Display() of Base class A";
+    }
+};
+class B:public A
+{
+public:
+    void display()
+    {
+        cout<<"Display method of derived class B";
+    }
+};
+int main()
+{
+    B obj;
+    A &bs=obj;    //pointer of object type
+    obj.display();
+
+}
